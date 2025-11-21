090da44c72f5cfacce8c8e52dccd8d867af51371
mikael.lanceong <mikael.lanceong@gmail.com>
2025-11-21 08:20:41 +0800

    Add changes to homepage subtitle

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -3,7 +3,7 @@
         <div class="row">
             <div class="col mt-5 pt-3 text-center mx-auto">
                 <h1 class="home-title py-3">MindBloggle</h1>
-                <h4 class="home-subtitle py-2">Boogle the Mind with Blogs!</h4>
+                <h4 class="home-subtitle py-2">Boggle the Mind with Blogs!</h4>
                 <router-link id="home-router" class="btn" :to="{name: 'Blogs'}">Check Some Blogs</router-link>
             </div>
         </div>
